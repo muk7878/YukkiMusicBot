1e947fecc2d92a3a68a273bc55e3ac914ec716ed
muk7878 <98019764+muk7878@users.noreply.github.com>
2022-02-02 07:01:08 +0700

    Update Lyrics.py

--- Yukki/Plugins/Lyrics.py
+++ Yukki/Plugins/Lyrics.py
@@ -8,8 +8,8 @@ from youtubesearchpython import VideosSearch
 
 from Yukki import MUSIC_BOT_NAME, app
 
-__mMODULE__ = "Lyrics"
-__mHELP__ = """
+__MODULE__ = "Lyrics"
+__HELP__ = """
 
 /Lyrics [Music Name]
 - Searches Lyrics for the particular Music on web.
